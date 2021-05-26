c5ad5a2289514ea3bb9b2b416177597563be6c2d
visheshgargavi <63608636+visheshgargavi@users.noreply.github.com>
2021-05-26 18:57:52 +0530

    Update model.py

--- task_1/model.py
+++ task_1/model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import joblib
-data=pd.read_csv('SalaryData')
+data=pd.read_csv('SalaryData.csv')
 X=data['YearsExperience'].values.reshape(-1,1)
 y=data['Salary']
 from sklearn.linear_model import LinearRegression
